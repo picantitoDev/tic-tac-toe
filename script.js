287397bbd055025c5ead4b84d9489269e034d62e
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-01-05 21:03:34 -0500

    I think Im understanding

--- script.js
+++ script.js
@@ -3,58 +3,47 @@ let gameBoardFactory = function () {
         ["", "", "",
             "", "", "",
             "", "", ""];
+
+    const winningPositions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [6, 4, 2]
+    ];
+
+    const isAWin = function (a, b, c) {
+        return board[a] === board[b] && board[b] === board[c];
+    };
+
     return {
         clearBoard() {
-            for (let i = 0; i < board.length; i++){
+            for (let i = 0; i < board.length; i++) {
                 board[i] = "";
             }
         },
 
         updateBoard(position, symbol) {
-            if(board[position] === ""){
-                board[position] = symbol;
+            if (position < 0 || position > 8 || board[position] !== "") {
+                return;
             }
+            board[position] = symbol;
         },
 
         checkWinner() {
-            let won = false;
-
-            // Horizontal Wins
-            if (board[0] === board[1] && board[1] === board[2] && board[0] === board[2]){
-                won = true;
+            for (let [a, b, c] of winningPositions) { 
+                if (isAWin(a, b, c)) {
+                    return true;  
+                }
             }
+            return false;
+        },
 
-            if (board[3] === board[4] && board[4] === board[5] && board[3] === board[5]){
-                won = true;
-            }
-
-            if (board[6] === board[7] && board[7] === board[8] && board[6] === board[8]){
-                won = true;
-            }
-
-            // Vertical Wins
-            if (board[0] === board[3] && board[3] === board[6] && board[0] === board[6]){
-                won = true;
-            }
-
-            if (board[1] === board[4] && board[4] === board[7] && board[1] === board[7]){
-                won = true;
-            }
-
-            if (board[2] === board[5] && board[5] === board[8] && board[2] === board[8]){
-                won = true;
-            }
-
-            // Diagonal Wins
-            if (board[0] === board[4] && board[4] === board[8] && board[0] === board[8]){
-                won = true;
-            }
-
-            if (board[6] === board[4] && board[4] === board[2] && board[6] === board[2]){
-                won = true;
-            }
-
-            return won;
+        checkDraw() {
+            return board.every(cell => cell !== "") && !this.checkWinner();
         }
     };
 }();
